9d5cd3462f801e0293bef62f89e2133f336ce200
Alessandro C <alessandro.christopher@gmail.com>
2016-10-17 13:55:36 +0700

    Submitted

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,13 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  var say = req.param('say')
-  res.render('say', {ss : say});
+  var say = req.param('string')
+  if(say == undefined){
+      say = ""
+  }else{
+     say = req.param('string').toLowerCase()
+  }
+  res.render('form', { say })
 });
 
 module.exports = router;
