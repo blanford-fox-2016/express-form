240df5cfe97b366c4afd1453e91e9601e26d1bb2
& <pair+ajilantang.tamatamvan@devbootcamp.com>
2016-10-17 14:47:36 +0700

    sedikit tambhan

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,5 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-router.post('/', (req, res, next) => {
-  res.send(req);
-})
 
 module.exports = router;
