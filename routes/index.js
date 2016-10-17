28a2a22ccc14c32cf8aa560952b676a5718eb3b8
Dharmadi Tanamas & Mangku Widodo <pair+dharmadi93.mangkuwi@hacktiv8.com>
2016-10-17 11:23:05 +0700

    update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // release 1
+  // var temp = req.query.say
+  // res.send(temp);
+  firstname = req.query.firstname
+  lastname = req.query.lastname
+
+  res.render('index', {
+    firstname:firstname,
+    lastname:lastname
+  });
+
 });
 
 module.exports = router;
